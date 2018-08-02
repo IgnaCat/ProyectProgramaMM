3fc35be79343d0985671e0a51d1632599e11bb57
IgnaCat <itsigna02@gmail.com>
2018-08-02 15:04:02 -0300

    Cree el inicio

---
+++ src/proyectoprogamamm/Cliente.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogamamm;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ignacio
+ */
+public class Cliente {
+    
+    private JFrame frame1;
+    
+    public Cliente() {
+        frame1 = new JFrame("Cliente M&M");
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame1.setSize(1400, 800);
+        frame1.getContentPane().setBackground(Color.lightGray);
+        frame1.setResizable(false);
+
+        frame1.setLayout(null);
+        
+
+        
+        
+        frame1.setVisible(
+                true);
+}
+}
---
+++ src/proyectoprogamamm/ProyectoProgramaMM.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogamamm;
+
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+/**
+ *
+ * @author ignacio
+ */
+public class ProyectoProgramaMM {
+    
+    private JFrame frame;
+
+    
+    public ProyectoProgramaMM(){
+        frame = new JFrame("M&M Gym");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1300, 800);
+        frame.getContentPane().setBackground(Color.lightGray);
+        frame.setResizable(false);
+
+        frame.setLayout(null);
+        
+        ImageIcon ii = new ImageIcon("/home/ignacio/Escritorio/ProyectProgramaMM/Imagen/gym.png");
+        JLabel lable = new JLabel(ii);
+        lable.setBounds(0, 0, 1400, 800);
+        
+        JButton btnCliente = new JButton("Cliente");
+        btnCliente.setBounds(200, 300, 180, 40);
+
+        JButton btnClases = new JButton("Clases");
+        btnClases.setBounds(200, 400, 180, 40);
+
+
+        frame.add(btnClases);
+        frame.add(btnCliente);
+        frame.add(lable);
+
+	 
+   
+        frame.setVisible(
+                true);
+        
+    }
+    
+   
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ProyectoProgramaMM MM = new ProyectoProgramaMM();
+
+
+    }
+    
+}
