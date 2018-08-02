488f0a7f21315fb6f65fd6c25ff1c8c2f51c821b
FranMartiarena <franciscomartiarena@gmail.com>
2018-08-02 15:15:33 -0300

    probando

--- src/proyectoprogamamm/ProyectoProgramaMM.java
+++ src/proyectoprogamamm/ProyectoProgramaMM.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectoprogamamm;
 
 import java.awt.Color;
@@ -12,10 +8,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 
-/**
- *
- * @author ignacio
- */
 public class ProyectoProgramaMM {
     
     private JFrame frame;
