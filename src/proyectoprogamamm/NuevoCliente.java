4c9435eefd73ad98beeb00f64cece1799907393a
IgnaCat <itsigna02@gmail.com>
2018-08-04 19:31:28 -0300

    Empeze la pag de nuevo cliente

--- src/proyectoprogamamm/NuevoCliente.java
+++ src/proyectoprogamamm/NuevoCliente.java
@@ -3,15 +3,35 @@ package proyectoprogamamm;
 
 import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 
 public class NuevoCliente {
     
     private JFrame frame2;
-    
+    private JLabel lblNombre;
+    private JLabel lblApellido;
+    private JLabel lblDni;
+    private JLabel lblFechaDia;
+    private JLabel lblFechaMes;
+    private JLabel lblFechaAño;
+    private JLabel lblProfesor;
+    private JLabel lblDireccion;
+    private JLabel lblTelefono;
+    private JTextField txtNombre;
+    private JTextField txtApellido;
+    private JTextField txtDni;
+    private JTextField txtFechaDia;
+    private JTextField txtFechaMes;
+    private JTextField txtFechaAño;
+    private JTextField txtProfesor;
+    private JTextField txtDireccion;
+    private JTextField txtTelefono;
+
     public NuevoCliente(){
         
-        frame2 = new JFrame(" Nuevo Cliente");
+        frame2 = new JFrame("Nuevo Cliente");
         frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame2.setSize(800, 600);
         frame2.getContentPane().setBackground(Color.lightGray);
@@ -19,10 +39,15 @@ public class NuevoCliente {
 
         frame2.setLayout(null);
         
+        lblNombre = new JLabel("Nombre");
+        lblNombre.setBounds(40, 30, 100, 30);
+        
+        txtNombre = new JTextField();
+        txtNombre.setBounds(110, 30, 160, 30);
         
         
-        
-        
+        frame2.add(txtNombre);
+        frame2.add(lblNombre);
         frame2.setVisible(
                 true);
     }
