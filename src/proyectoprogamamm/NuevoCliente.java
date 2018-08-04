319da00cd6be146b2b0053ab931d213d4d4ad259
IgnaCat <itsigna02@gmail.com>
2018-08-04 16:25:22 -0300

    Cambie una boludes

--- src/proyectoprogamamm/NuevoCliente.java
+++ src/proyectoprogamamm/NuevoCliente.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectoprogamamm;
 
 import java.awt.Color;
 import javax.swing.JFrame;
 
-/**
- *
- * @author ignacio
- */
+
 public class NuevoCliente {
     
     private JFrame frame2;
@@ -19,7 +12,7 @@ public class NuevoCliente {
     public NuevoCliente(){
         
         frame2 = new JFrame(" Nuevo Cliente");
-        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame2.setSize(800, 600);
         frame2.getContentPane().setBackground(Color.lightGray);
         frame2.setResizable(false);
