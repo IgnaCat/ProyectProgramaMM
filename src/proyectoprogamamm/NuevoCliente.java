64d1ccdbad466fd7f9fc08175be1ce6ce6ca8e21
FranMartiarena <franciscomartiarena@gmail.com>
2018-08-06 20:34:06 -0300

    f

--- src/proyectoprogamamm/NuevoCliente.java
+++ src/proyectoprogamamm/NuevoCliente.java
@@ -2,6 +2,7 @@
 package proyectoprogamamm;
 
 import java.awt.Color;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -28,13 +29,16 @@ public class NuevoCliente {
     private JTextField txtProfesor;
     private JTextField txtDireccion;
     private JTextField txtTelefono;
+    private JLabel lblFecha;
+    private JButton btnCancelar;
+    private JButton btnAgregar;
 
     public NuevoCliente(){
         
         frame2 = new JFrame("Nuevo Cliente");
         frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame2.setSize(800, 600);
-        frame2.getContentPane().setBackground(Color.lightGray);
+        frame2.getContentPane().setBackground(Color.ORANGE);
         frame2.setResizable(false);
 
         frame2.setLayout(null);
@@ -42,12 +46,75 @@ public class NuevoCliente {
         lblNombre = new JLabel("Nombre");
         lblNombre.setBounds(40, 30, 100, 30);
         
+        lblApellido = new JLabel("Apellido");
+        lblApellido.setBounds(40, 30, 100, 150);
+        
+        lblTelefono = new JLabel("Telefono");
+        lblTelefono.setBounds(40, 30, 100, 270);
+        
+        lblDireccion = new JLabel("Direccion");
+        lblDireccion.setBounds(40, 30, 100, 390);
+        
+        lblFechaAño = new JLabel("Año");
+        lblFechaAño.setBounds(300, 30, 100, 510);
+        
+        lblFechaDia = new JLabel("Dia");
+        lblFechaDia.setBounds(100, 30, 100, 510);
+        
+        lblFechaMes = new JLabel("Mes");
+        lblFechaMes.setBounds(200, 30, 100, 510);
+        
+        lblFecha = new JLabel("Fecha");
+        lblFecha.setBounds(40, 30, 100, 510);
+        
         txtNombre = new JTextField();
         txtNombre.setBounds(110, 30, 160, 30);
+        
+        txtApellido = new JTextField();
+        txtApellido.setBounds(110, 90, 160, 30);
+        
+        txtTelefono = new JTextField();
+        txtTelefono.setBounds(110, 150, 160, 30);
+        
+        txtDireccion = new JTextField();
+        txtDireccion.setBounds(110, 210, 160, 30);
+        
+        txtFechaDia = new JTextField();
+        txtFechaDia.setBounds(130, 270, 60, 30);
+        
+        txtFechaMes = new JTextField();
+        txtFechaMes.setBounds(235, 270, 60, 30);
+        
+        txtFechaAño = new JTextField();
+        txtFechaAño.setBounds(340, 270, 60, 30);
+        
+        btnCancelar = new JButton("Cancelar");
+        btnCancelar.setBounds(450, 270, 100, 25);
+        
+        btnAgregar = new JButton("Agregar");
+        btnAgregar.setBounds(600, 270, 100, 25);
+        
+       
         
         
         frame2.add(txtNombre);
         frame2.add(lblNombre);
+        frame2.add(lblApellido);
+        frame2.add(txtApellido);
+        frame2.add(lblTelefono);
+        frame2.add(txtTelefono);
+        frame2.add(lblDireccion);
+        frame2.add(txtDireccion);
+        frame2.add(lblFecha);
+        frame2.add(lblFechaAño);
+        frame2.add(lblFechaDia);
+        frame2.add(lblFechaMes);
+        frame2.add(txtFechaMes);
+        frame2.add(txtFechaDia);
+        frame2.add(txtFechaAño);
+        frame2.add(btnAgregar);
+        frame2.add(btnCancelar);
+        
         frame2.setVisible(
                 true);
     }
