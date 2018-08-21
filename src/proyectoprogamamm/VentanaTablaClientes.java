5729f275f0fc7805f7e3c1eb1ed3f5f2dc53aedb
FranMartiarena <franciscomartiarena@gmail.com>
2018-08-21 13:33:35 -0300

    Corregims error de metodo guardar

--- src/proyectoprogamamm/VentanaTablaClientes.java
+++ src/proyectoprogamamm/VentanaTablaClientes.java
@@ -34,25 +34,24 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
+public class VentanaTablaClientes {
 
-
-public class VentanaTablaClientes{
-    
     private JFrame frame1;
     private JTable table;
     private JScrollPane scroll;
     private JTextField txtBuscar;
-    private DefaultTableModel model = new DefaultTableModel();;
+    private DefaultTableModel model = new DefaultTableModel();
+    ;
     private VentanaTablaClientes me = this;
     private JComboBox comboBuscar;
     private JLabel lblBuscar;
     private TableRowSorter trsFiltro;
-    private File file;
     private String nom_files;
     private Cliente cliente;
-    
+    private static final String file = "Gym/gym.txt";
+    private static final String vacio = "empty";
+
 //    private static NuevoCliente nuevoCliente = new NuevoCliente();
-    
     public VentanaTablaClientes() {
         frame1 = new JFrame("Cliente");
         frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -61,9 +60,7 @@ public class VentanaTablaClientes{
         frame1.setResizable(false);
 
         frame1.setLayout(null);
-        
-        
-        
+
         table = new JTable(model);
         model.addColumn("Nombre");
         model.addColumn("Apellido");
@@ -72,19 +69,18 @@ public class VentanaTablaClientes{
         model.addColumn("Profesor");
         model.addColumn("Direccion");
         model.addColumn("Telefono");
-        
+
 //        model.addRow(new Object[]{"lol"});
-        
         table.setFont(new java.awt.Font("Tahoma", 30, 15));
         table.setRowHeight(25);
 //        table.getColumnModel().getColumn(0).setPreferredWidth(200);       
 
         scroll = new JScrollPane(table);
         scroll.setBounds(400, 25, 840, 700);
-        
+
         lblBuscar = new JLabel("Buscar por:");
-        lblBuscar.setBounds(120,60,200,30);
-        
+        lblBuscar.setBounds(120, 60, 200, 30);
+
         comboBuscar = new JComboBox();
         comboBuscar.addItem("Nombre");
         comboBuscar.addItem("Apellido");
@@ -93,67 +89,65 @@ public class VentanaTablaClientes{
         comboBuscar.addItem("Profesor");
         comboBuscar.addItem("Direccion");
         comboBuscar.addItem("Telefono");
-        comboBuscar.setBounds(120,100,200,30);
-        
+        comboBuscar.setBounds(120, 100, 200, 30);
+
         txtBuscar = new JTextField();
         txtBuscar.setToolTipText("Busque algo aqui");
         txtBuscar.setBounds(120, 150, 200, 35);
-        
-        
+
         JButton btnBuscar = new JButton("Buscar");
         btnBuscar.setBounds(120, 200, 200, 30);
-        
+
         JButton btnNuevoCliente = new JButton("Nuevo Cliente");
         btnNuevoCliente.setBounds(120, 400, 200, 30);
-        
+
         JButton btnEliminar = new JButton("Eliminar ");
         btnEliminar.setBounds(120, 240, 200, 30);
-        
+
         JButton btnVolver = new JButton("Volver");
         btnVolver.setBounds(120, 450, 200, 30);
-        
+
         JButton btnExportar = new JButton("Guardar");
         btnExportar.setBounds(120, 500, 200, 30);
-        
+
         btnExportar.addActionListener((ActionEvent e) -> {
             guardar();
         });
 
-        
-         btnVolver.addActionListener(new ActionListener() {
+        btnVolver.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Inicio inicio = new Inicio();
                 frame1.setVisible(false);
             }
         });
-        
+
         btnEliminar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int fila = table.getSelectedRow();
                 if (fila >= 0) {
-                       model.removeRow(fila);
-        }
-                
+                    model.removeRow(fila);
+                }
+
             }
         });
-        
+
         btnNuevoCliente.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 NuevoCliente nuevoCliente = new NuevoCliente(me);
-                
+
             }
         });
-        
+
         btnBuscar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-               buscar(); 
+                buscar();
             }
         });
-           
+
         frame1.add(btnNuevoCliente);
         frame1.add(lblBuscar);
         frame1.add(comboBuscar);
@@ -163,16 +157,12 @@ public class VentanaTablaClientes{
         frame1.add(btnEliminar);
         frame1.add(btnVolver);
         frame1.add(btnExportar);
-        
-        
+
         frame1.setVisible(
                 true);
-        
-        
-        
+
 //        nuevoCliente.setVisible(false);
 //        mm.cerrarNuevoCliente();
-        
     }
 
     public JFrame getFrame1() {
@@ -194,22 +184,22 @@ public class VentanaTablaClientes{
     public JTextField getTxtBuscar() {
         return txtBuscar;
     }
-    
-    public void setVisible(boolean b){
+
+    public void setVisible(boolean b) {
         frame1.setVisible(b);
     }
 
     void addClienteRow(String[] cliente) {
         this.getModel().addRow(cliente);
     }
-    
-    public void buscar(){
-        String cadena = (txtBuscar.getText());               
+
+    public void buscar() {
+        String cadena = (txtBuscar.getText());
         trsFiltro = new TableRowSorter(table.getModel());
         table.setRowSorter(trsFiltro);
         filtro();
     }
-    
+
     public void filtro() {
         int columnaABuscar = 0;
         if (comboBuscar.getSelectedItem().toString() == "Nombre") {
@@ -234,61 +224,48 @@ public class VentanaTablaClientes{
             columnaABuscar = 6;
         }
         trsFiltro.setRowFilter(RowFilter.regexFilter(txtBuscar.getText(), columnaABuscar));
-}
-    
-    public void guardar(){
-        JFileChooser jF1 = new JFileChooser();
-            String ruta = "Gym/gym.txt";
-//            try {
-//                if (jF1.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
-//                    ruta = jF1.getSelectedFile().getAbsolutePath(); // /home/.gym
-//                }
-//            } catch (HeadlessException ex) {
-//                ex.printStackTrace();
-//            }
-
-            File filename = new File(ruta);
-            
-
-
-            JFrame frame1 = new JFrame();
-
-            try {
-
-                try (BufferedWriter bfw = new BufferedWriter(new FileWriter(filename))) {
-//                    for(int i = 0; i<contenido.length;i++){ 
-//                        fileOut.write(contenido[i] + " - ");
-//                    }
-                 for (int i = 0 ; i < table.getRowCount(); i++) 
-            {
-                for(int j = 0 ; j < table.getColumnCount();j++) 
-                {
-                    bfw.write((String)(table.getValueAt(i,j)));
-                    if (j < table.getColumnCount() -1) { 
-                        bfw.write(" - ");
-                    }
-                }
-                bfw.newLine(); 
-            }
-                 bfw.close();
-
-                }
-
-                JOptionPane.showMessageDialog(frame1, "Su archivo ha sido guardado");
-                
-
-            } catch (IOException ioe) {
-                System.out.println("Exception Caught : " + ioe.getMessage());
-            }
     }
-    
+
+    public void guardar() {
+        File filename = new File(file);
+        String carga;
+        try {
+            
+            try (BufferedWriter bfw = new BufferedWriter(new FileWriter(filename))) {
+
+                for (int i = 0; i < table.getRowCount(); i++) {
+                    for (int j = 0; j < table.getColumnCount(); j++) {
+                        carga = (String) table.getValueAt(i, j);
+                        System.out.println(carga);
+                        try {
+                            bfw.write(carga);
+                        }catch (NullPointerException p) {
+                            bfw.write(vacio);
+                        }
+                        if (j < table.getColumnCount() - 1) {
+                            bfw.write(" - ");
+                        }
+                    }
+                    bfw.newLine();
+                }
+                bfw.close();
+
+            }
+
+            JOptionPane.showMessageDialog(frame1, "Su archivo ha sido guardado");
+
+        } catch (IOException ioe) {
+            System.out.println("Exception Caught : " + ioe.getMessage());
+        }
+    }
+
     public void importar() {
-        
+
         String line = null;
         DefaultTableModel dtm = (DefaultTableModel) table.getModel();
-        
+
         try {
-            String file = "Gym/gym.txt";
+            
             BufferedReader br = new BufferedReader(new FileReader(file));
 
             while ((line = br.readLine()) != null) {
@@ -296,7 +273,11 @@ public class VentanaTablaClientes{
                 StringTokenizer st1 = new StringTokenizer(line, " - ");
                 while (st1.hasMoreTokens()) {
                     String nextToken = st1.nextToken();
-                    data.add(nextToken);
+                    if (!nextToken.equals(vacio)){
+                        data.add(nextToken);
+                    } else {
+                        data.add(null);
+                    }
                     System.out.println(nextToken);
 
                 }
@@ -312,9 +293,5 @@ public class VentanaTablaClientes{
         }
 
     }
-
-    
-
-    
 
 }
